44151f0916e7b7a0555b09e023a51debb90ffa0b
raininsnovv <raininsnovv@gmail.com>
2023-11-14 08:31:47 +0300

    tetris demo

---
+++ src/Tetris.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './components/Board';
+import './App.css'
+
+const Tetris = () => {
+    return (
+        <div className="t-parent">
+            <Board/>
+        </div>
+    );
+};
+
+export default Tetris;
